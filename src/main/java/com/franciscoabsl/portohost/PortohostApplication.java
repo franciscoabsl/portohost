3221c95e249fd0676bd12ca918d7cb1eea87fe6f
franciscoabsl <franciscosouzalima@gmail.com>
2024-09-09 19:32:27 -0300

    project start and db setup

---
+++ src/main/java/com/franciscoabsl/portohost/PortohostApplication.java
@@ -0,0 +1,13 @@
+package com.franciscoabsl.portohost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortohostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortohostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/franciscoabsl/portohost/PortohostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franciscoabsl.portohost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortohostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
